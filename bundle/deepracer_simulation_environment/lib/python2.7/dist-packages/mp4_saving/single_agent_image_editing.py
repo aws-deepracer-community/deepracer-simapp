423f254ad637670cdc21e38ff2a2461df2022d95
Lars Ludvigsen <lars@ludvig.no>
2020-03-29 15:38:05 +0000

    Second round - clean changes

--- bundle/deepracer_simulation_environment/lib/python2.7/dist-packages/mp4_saving/single_agent_image_editing.py
+++ bundle/deepracer_simulation_environment/lib/python2.7/dist-packages/mp4_saving/single_agent_image_editing.py
@@ -6,15 +6,14 @@ import logging
 import rospy
 import cv2
 from sensor_msgs.msg import Image as ROSImg
-
-from markov.utils import Logger
+from markov.log_handler.logger import Logger
 from markov.rospy_wrappers import ServiceProxyWrapper
 from deepracer_simulation_environment.srv import VideoMetricsSrvRequest, VideoMetricsSrv
 from mp4_saving import utils
 from mp4_saving.constants import (RaceCarColorToRGB,
                                   IconographicImageSize,
-                                  TrackAssetsIconographicPngs,
-                                  RACE_TYPE_TO_VIDEO_TEXT_MAPPING)
+                                  TrackAssetsIconographicPngs, RACE_COMPLETE_Y_OFFSET,
+                                  RACE_TYPE_TO_VIDEO_TEXT_MAPPING, XYPixelLoc, AWS_DEEPRACER_WATER_MARK)
 from mp4_saving.image_editing_interface import ImageEditingInterface
 from mp4_saving.top_view_graphics import TopViewGraphics
 
@@ -34,14 +33,21 @@ class SingleAgentImageEditing(ImageEditingInterface):
         self.race_type = race_type
         # Store the font which we will use to write the phase with
         self.amazon_ember_regular_20px = utils.get_font('AmazonEmber-Regular', 20)
+        self.amazon_ember_regular_16px = utils.get_font('AmazonEmber-Regular', 16)
         self.amazon_ember_heavy_30px = utils.get_font('AmazonEmber-Heavy', 30)
         self.amazon_ember_light_18px = utils.get_font('AmazonEmber-Light', 18)
         self.amazon_ember_light_20px = utils.get_font('AmazonEmber-Light', 20)
         self.amazon_ember_light_italic_20px = utils.get_font('AmazonEmber-LightItalic', 20)
 
+        self.is_racing = rospy.get_param("VIDEO_JOB_TYPE", "") == "RACING"
+        self.is_league_leaderboard = rospy.get_param("LEADERBOARD_TYPE", "") == "LEAGUE"
+        self.leaderboard_name = rospy.get_param("LEADERBOARD_NAME", "")
+
         # The track image as iconography
         self.track_icongraphy_img = utils.get_track_iconography_image()
-        self.gradient_img = utils.get_image(TrackAssetsIconographicPngs.OBSTACLE_OVERLAY_PNG.value,
+        gradient_img_path = TrackAssetsIconographicPngs.OBSTACLE_OVERLAY_PNG_LEAGUE_LEADERBOARD.value \
+            if self.is_league_leaderboard else TrackAssetsIconographicPngs.OBSTACLE_OVERLAY_PNG.value
+        self.gradient_img = utils.get_image(gradient_img_path,
                                             IconographicImageSize.FULL_IMAGE_SIZE.value)
         self.gradient_alpha = self.gradient_img[:, :, 3] / 255.0
 
@@ -69,7 +75,7 @@ class SingleAgentImageEditing(ImageEditingInterface):
         mp4_video_metrics_info = self.mp4_video_metrics_srv(VideoMetricsSrvRequest())
 
         # Top left location of the picture
-        loc_x, loc_y = (10, 10)
+        loc_x, loc_y = XYPixelLoc.SINGLE_AGENT_DISPLAY_NAME_LOC.value
 
         # Display name (Racer name/Model name)
         display_name = self.racecar_info['display_name']
@@ -104,29 +110,49 @@ class SingleAgentImageEditing(ImageEditingInterface):
                                                    font_color=RaceCarColorToRGB.White.value,
                                                    font_shadow_color=RaceCarColorToRGB.Black.value)
         # Speed
-        loc_y = 420
+        loc_x, loc_y = XYPixelLoc.SPEED_EVAL_LOC.value
+        if self.is_league_leaderboard:
+            loc_x, loc_y = XYPixelLoc.SPEED_LEADERBOARD_LOC.value
         speed_text = "{} m/s".format(utils.get_speed_formatted_str(mp4_video_metrics_info.throttle))
         major_cv_image = utils.write_text_on_image(image=major_cv_image, text=speed_text,
                                                    loc=(loc_x, loc_y), font=self.amazon_ember_light_20px,
                                                    font_color=RaceCarColorToRGB.White.value,
                                                    font_shadow_color=RaceCarColorToRGB.Black.value)
+        # Leaderboard name
+        if self.is_league_leaderboard:
+            loc_x, loc_y = XYPixelLoc.LEADERBOARD_NAME_LOC.value
+            major_cv_image = utils.write_text_on_image(image=major_cv_image, text=self.leaderboard_name,
+                                                       loc=(loc_x, loc_y), font=self.amazon_ember_regular_16px,
+                                                       font_color=RaceCarColorToRGB.White.value,
+                                                       font_shadow_color=RaceCarColorToRGB.Black.value)
         # Evaluation type
-        loc_y += 25
-        # TODO - Show text based on whether its a race or customer run evaluation
-        race_text = "race"
+        loc_x, loc_y = XYPixelLoc.RACE_TYPE_EVAL_LOC.value
+        if self.is_league_leaderboard:
+            loc_x, loc_y = XYPixelLoc.RACE_TYPE_RACE_LOC.value
+        race_text = "race" if self.is_racing else "evaluation"
         evaluation_type_txt = "{} {}".format(RACE_TYPE_TO_VIDEO_TEXT_MAPPING[self.race_type], race_text)
         major_cv_image = utils.write_text_on_image(image=major_cv_image, text=evaluation_type_txt,
                                                    loc=(loc_x, loc_y), font=self.amazon_ember_light_italic_20px,
                                                    font_color=RaceCarColorToRGB.White.value,
                                                    font_shadow_color=RaceCarColorToRGB.Black.value)
+
+        # AWS Deepracer logo at the bottom for the community leaderboard
+        if self.is_league_leaderboard:
+            major_cv_image = utils.write_text_on_image(image=major_cv_image, text=AWS_DEEPRACER_WATER_MARK,
+                                                       loc=XYPixelLoc.AWS_DEEPRACER_WATER_MARK_LOC.value,
+                                                       font=self.amazon_ember_regular_16px,
+                                                       font_color=RaceCarColorToRGB.White.value,
+                                                       font_shadow_color=RaceCarColorToRGB.Black.value)
+
         # Check if the done flag is set and set the banner appropriately
         if mp4_video_metrics_info.done and (int(total_laps) == current_lap):
             # When the cv2 text is written, it automatically drops the alpha value of the image
+            rel_y_offset = XYPixelLoc.TRACK_IMG_WITH_OFFSET_LOC.value[1] if self.is_league_leaderboard else 0
             major_cv_image = cv2.cvtColor(major_cv_image, cv2.COLOR_RGB2RGBA)
             racecomplete_image = utils.get_image(TrackAssetsIconographicPngs.RACE_COMPLETE_OVERLAY_PNG.value,
                                                  IconographicImageSize.RACE_COMPLETE_IMAGE_SIZE.value)
             x_offset = major_cv_image.shape[1] - racecomplete_image.shape[1]//2
-            y_offset = major_cv_image.shape[0] - 180 - racecomplete_image.shape[0]//2
+            y_offset = major_cv_image.shape[0] - RACE_COMPLETE_Y_OFFSET - rel_y_offset - racecomplete_image.shape[0]//2
             major_cv_image = utils.plot_rectangular_image_on_main_image(
                 major_cv_image, racecomplete_image, (x_offset, y_offset))
         return major_cv_image
@@ -141,4 +167,6 @@ class SingleAgentImageEditing(ImageEditingInterface):
     def edit_image(self, major_cv_image):
         major_cv_image = self._edit_major_cv_image(major_cv_image)
         minor_cv_image = self._edit_minor_cv_image()
-        return utils.overlay_track_images(major_cv_image, minor_cv_image)
+        offset_loc = XYPixelLoc.TRACK_IMG_WITH_OFFSET_LOC.value if self.is_league_leaderboard \
+            else XYPixelLoc.TRACK_IMG_WITHOUT_OFFSET_LOC.value
+        return utils.overlay_track_images(major_cv_image, minor_cv_image, offset_loc)
