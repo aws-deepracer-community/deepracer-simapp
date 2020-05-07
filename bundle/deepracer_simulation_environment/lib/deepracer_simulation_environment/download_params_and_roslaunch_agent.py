f7be1ce41e03e142a4748de24029f79a39adf9b9
Lars Lorentz Ludvigsen <larsll@outlook.com>
2020-05-07 20:39:20 +0000

    Merged files

--- bundle/deepracer_simulation_environment/lib/deepracer_simulation_environment/download_params_and_roslaunch_agent.py
+++ bundle/deepracer_simulation_environment/lib/deepracer_simulation_environment/download_params_and_roslaunch_agent.py
@@ -6,7 +6,6 @@ Example:
     $ python download_params_and_roslaunch_agent.py s3_region, s3_bucket, s3_prefix, s3_yaml_name, launch_name
 """
 
-# import argparse
 import logging
 from subprocess import Popen
 import os
@@ -17,7 +16,8 @@ import boto3
 import yaml
 import rospy
 from markov import utils_parse_model_metadata
-from markov.utils import force_list
+from markov.rollout_constants import BodyShellType
+from markov.utils import force_list, test_internet_connection
 from markov.constants import DEFAULT_COLOR
 from markov.architecture.constants import Input
 from markov.utils import  get_boto_config
@@ -30,25 +30,33 @@ LOG = Logger(__name__, logging.INFO).get_logger()
 
 # Pass a list with 2 values for CAR_COLOR, MODEL_S3_BUCKET, MODEL_S3_PREFIX, MODEL_METADATA_FILE_S3_KEY for multicar
 CAR_COLOR_YAML_KEY = "CAR_COLOR"
+BODY_SHELL_TYPE_YAML_KEY = "BODY_SHELL_TYPE"
 RACE_TYPE_YAML_KEY = "RACE_TYPE"
-HEAD_TO_MODEL_RACE_TYPE = "HEAD_TO_MODEL"
-TIME_TRIAL_RACE_TYPE = "TIME_TRIAL"
 MODEL_S3_BUCKET_YAML_KEY = "MODEL_S3_BUCKET"
 MODEL_S3_PREFIX_YAML_KEY = "MODEL_S3_PREFIX"
 MODEL_METADATA_FILE_S3_YAML_KEY = "MODEL_METADATA_FILE_S3_KEY"
+RACER_NAME_YAML_KEY = "RACER_NAME"
+
+TIME_TRIAL_RACE_TYPE = "TIME_TRIAL"
+F1_RACE_TYPE = "F1"
 # Amount of time to wait to guarantee that RoboMaker's network configuration is ready.
 WAIT_FOR_ROBOMAKER_TIME = 10
 
+# User alias for F1 shell
+F1_SHELL_USERS_LIST = ["TataCalde", "RIC3", "SheBangsTheDrums1989"]
+
 def main():
     """ Main function for downloading yaml params """
-    try:
-        # parse argument
-        s3_region = sys.argv[1]
-        s3_bucket = sys.argv[2]
-        s3_prefix = sys.argv[3]
-        s3_yaml_name = sys.argv[4]
-        launch_name = sys.argv[5]
 
+    # parse argument
+    s3_region = sys.argv[1]
+    s3_bucket = sys.argv[2]
+    s3_prefix = sys.argv[3]
+    s3_yaml_name = sys.argv[4]
+    launch_name = sys.argv[5]
+    yaml_key = os.path.normpath(os.path.join(s3_prefix, s3_yaml_name))
+
+    try:
         # create boto3 session/client and download yaml/json file
         session = boto3.session.Session()
 
@@ -57,19 +65,10 @@ def main():
         if s3_endpoint_url is not None:
             LOG.info('Endpoint URL {}'.format(s3_endpoint_url))
             rospy.set_param('S3_ENDPOINT_URL', s3_endpoint_url)
-        else:
-             # create boto3 session/client and download yaml/json file
-            ec2_client = session.client('ec2', s3_region)
-            LOG.info('Checking internet connection...')
-            response = ec2_client.describe_vpcs()
-            if not response['Vpcs']:
-                log_and_exit("No VPC attached to instance", SIMAPP_SIMULATION_WORKER_EXCEPTION,
-                             SIMAPP_EVENT_ERROR_CODE_500)
-            LOG.info('Verified internet connection')
+
 
         s3_client = session.client('s3', region_name=s3_region, endpoint_url=s3_endpoint_url, config=get_boto_config())
 
-        yaml_key = os.path.normpath(os.path.join(s3_prefix, s3_yaml_name))
         local_yaml_path = os.path.abspath(os.path.join(os.getcwd(), s3_yaml_name))
         s3_client.download_file(Bucket=s3_bucket, Key=yaml_key, Filename=local_yaml_path)
         # Get values passed in yaml files. Default values are for backward compatibility and for single racecar racing
@@ -77,13 +76,15 @@ def main():
                                MODEL_S3_BUCKET_YAML_KEY: s3_bucket,
                                MODEL_S3_PREFIX_YAML_KEY: s3_prefix,
                                CAR_COLOR_YAML_KEY: DEFAULT_COLOR,
-                               MODEL_METADATA_FILE_S3_YAML_KEY: None}
+                               BODY_SHELL_TYPE_YAML_KEY: None,
+                               MODEL_METADATA_FILE_S3_YAML_KEY: None,
+                               RACER_NAME_YAML_KEY: None}
         yaml_dict = get_yaml_dict(local_yaml_path)
         yaml_values = get_yaml_values(yaml_dict, default_yaml_values)
 
         # Forcing the yaml parameter to list
         force_list_params = [MODEL_METADATA_FILE_S3_YAML_KEY, MODEL_S3_BUCKET_YAML_KEY, MODEL_S3_PREFIX_YAML_KEY,
-                             CAR_COLOR_YAML_KEY]
+                             CAR_COLOR_YAML_KEY, BODY_SHELL_TYPE_YAML_KEY, RACER_NAME_YAML_KEY]
 
         for params in force_list_params:
             yaml_values[params] = force_list(yaml_values[params])
@@ -96,8 +97,12 @@ def main():
             for s3_prefix in yaml_values[MODEL_S3_PREFIX_YAML_KEY]:
                 yaml_values[MODEL_METADATA_FILE_S3_YAML_KEY].append(os.path.join(s3_prefix, 'model/model_metadata.json'))
 
-        # Set multicar value if its a head to model racetype
-        multicar = yaml_values[RACE_TYPE_YAML_KEY] == HEAD_TO_MODEL_RACE_TYPE
+        # Set multicar value if there is more than one value in MODEL_S3_BUCKET_YAML_KEY.
+        multicar = len(yaml_values[MODEL_S3_BUCKET_YAML_KEY]) > 1
+
+        # Set f1 as true if RACE_TYPE is F1
+        is_f1 = yaml_values[RACE_TYPE_YAML_KEY] == F1_RACE_TYPE
+
         # Validate the yaml values
         validate_yaml_values(yaml_values, multicar)
         # List of racecar names that should include second camera while launching
@@ -108,7 +113,22 @@ def main():
 
         # List of SimApp versions
         simapp_versions = list()
-
+        # List of body shell types
+        body_shell_types = yaml_values[BODY_SHELL_TYPE_YAML_KEY]
+        racer_names = yaml_values[RACER_NAME_YAML_KEY]
+        if None in body_shell_types:
+            # use default shells
+            if None in racer_names:
+                body_shell_types = [BodyShellType.DEFAULT.value] * len(yaml_values[MODEL_S3_BUCKET_YAML_KEY])
+            # use default shells for regular user and f1 shell for users in F1_SHELL_USERS_LIST
+            else:
+                body_shell_types = [BodyShellType.F1_2021.value if racer_alias in F1_SHELL_USERS_LIST
+                                    else BodyShellType.DEFAULT.value
+                                    for racer_alias in yaml_values[RACER_NAME_YAML_KEY]]
+                yaml_dict[BODY_SHELL_TYPE_YAML_KEY] = body_shell_types
+                # override local yaml file with updated BODY_SHELL_TYPE
+                with open(local_yaml_path, 'w') as yaml_file:
+                    yaml.dump(yaml_dict, yaml_file)
         for agent_index, model_s3_bucket in enumerate(yaml_values[MODEL_S3_BUCKET_YAML_KEY]):
 
             racecar_name = 'racecar_'+str(agent_index) if len(yaml_values[MODEL_S3_BUCKET_YAML_KEY]) > 1 else 'racecar'
@@ -129,10 +149,12 @@ def main():
 
         cmd = [''.join(("roslaunch deepracer_simulation_environment {} ".format(launch_name),
                         "local_yaml_path:={} ".format(local_yaml_path),
-                         "racecars_with_stereo_cameras:={} ".format(','.join(racecars_with_stereo_cameras)),
-                        "racecars_with_lidars:={} multicar:={} ".format(','.join(racecars_with_lidars), multicar),
-                        "car_colors:={} simapp_versions:={}".format(','.join(yaml_values[CAR_COLOR_YAML_KEY]),
-                                                                    ','.join(simapp_versions))))]
+                        "racecars_with_stereo_cameras:={} ".format(','.join(racecars_with_stereo_cameras)),
+                        "racecars_with_lidars:={} ".format(','.join(racecars_with_lidars)),
+                        "multicar:={} ".format(multicar),
+                        "body_shell_types:={} ".format(','.join(body_shell_types)),
+                        "simapp_versions:={} ".format(','.join(simapp_versions)),
+                        "f1:={}".format(is_f1)))]
         Popen(cmd, shell=True, executable="/bin/bash")
     
     except botocore.exceptions.ClientError as ex:
@@ -160,14 +182,14 @@ def validate_yaml_values(yaml_values, multicar):
         Exception -- [Exception]
     """
     # Verify if all the yaml keys required for launching models have same number of values
-    same_len_values = [MODEL_S3_BUCKET_YAML_KEY, MODEL_S3_PREFIX_YAML_KEY, MODEL_METADATA_FILE_S3_YAML_KEY,
+    same_len_values = [MODEL_S3_BUCKET_YAML_KEY, MODEL_S3_PREFIX_YAML_KEY,
                        CAR_COLOR_YAML_KEY]
     LOG.info(yaml_values)
     if not all(map(lambda param: len(yaml_values[param]) == len(yaml_values[same_len_values[0]]), same_len_values)):
         raise Exception('Incorrect number of values for these yaml parameters {}'.format(same_len_values))
 
-    # Verify if all yaml keys have 2 values for multi car racing
-    if multicar and len(yaml_values[MODEL_S3_PREFIX_YAML_KEY]) != 2:
+    # Verify if all yaml keys have at least 2 values for multi car racing
+    if multicar and len(yaml_values[MODEL_S3_PREFIX_YAML_KEY]) < 2:
         raise Exception('Incorrect number of values for multicar racing yaml parameters {}'.format(same_len_values))
 
     # Verify if all yaml keys have 1 value for single car racing
@@ -200,7 +222,6 @@ def get_yaml_values(yaml_dict, default_vals=None):
         log_and_exit("yaml read error: {}".format(exc),
                      SIMAPP_SIMULATION_WORKER_EXCEPTION, 
                      SIMAPP_EVENT_ERROR_CODE_500)
-
 
 
 if __name__ == '__main__':
