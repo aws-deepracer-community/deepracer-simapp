6f205faa8e01f2a44bf693c4b39770d74130a214
Lars Lorentz Ludvigsen <larsll@outlook.com>
2020-07-03 18:00:39 +0000

    MP4 updates

--- bundle/deepracer_simulation_environment/lib/python2.7/dist-packages/mp4_saving/multi_agent_image_editing.py
+++ bundle/deepracer_simulation_environment/lib/python2.7/dist-packages/mp4_saving/multi_agent_image_editing.py
@@ -4,19 +4,16 @@ import datetime
 import logging
 import rospy
 import cv2
-from cv_bridge import CvBridge
-from sensor_msgs.msg import Image as ROSImg
 
 from markov.log_handler.logger import Logger
-from markov.rospy_wrappers import ServiceProxyWrapper
 from markov.reset.constants import RaceType
+from markov.utils import get_racecar_idx
 from mp4_saving.top_view_graphics import TopViewGraphics
-from deepracer_simulation_environment.srv import VideoMetricsSrvRequest, VideoMetricsSrv
 from mp4_saving.constants import (RaceCarColorToRGB,
-                                  RACE_TYPE_TO_VIDEO_TEXT_MAPPING,
+                                  RACE_TYPE_TO_VIDEO_TEXT_MAPPING, SCALE_RATIO,
                                   TrackAssetsIconographicPngs, IconographicImageSize,
-                                  XYPixelLoc, AWS_DEEPRACER_WATER_MARK, RACE_COMPLETE_Y_OFFSET)
-
+                                  XYPixelLoc, AWS_DEEPRACER_WATER_MARK, RACE_COMPLETE_Y_OFFSET,
+                                  FrameQueueData)
 from mp4_saving import utils
 from mp4_saving.image_editing_interface import ImageEditingInterface
 
@@ -34,9 +31,10 @@ class MultiAgentImageEditing(ImageEditingInterface):
         """
         self.racecar_name = racecar_name
         self.racecars_info = racecars_info
+        racecar_index = get_racecar_idx(racecar_name)
+        self.racecar_index = racecar_index if racecar_index else 0
         self.race_type = race_type
-        # init cv bridge
-        self.bridge = CvBridge()
+
         # Store the font which we will use to write the phase with
         self.amazon_ember_regular_20px = utils.get_font('AmazonEmber-Regular', 20)
         self.amazon_ember_regular_16px = utils.get_font('AmazonEmber-Regular', 16)
@@ -51,27 +49,23 @@ class MultiAgentImageEditing(ImageEditingInterface):
 
         # The track image as iconography
         self.track_icongraphy_img = utils.get_track_iconography_image()
+
+        # Track image offset
+        self.track_loc_offset = XYPixelLoc.TRACK_IMG_WITH_OFFSET_LOC.value if self.is_league_leaderboard \
+            else XYPixelLoc.TRACK_IMG_WITHOUT_OFFSET_LOC.value
+
         gradient_img_path = TrackAssetsIconographicPngs.HEAD_TO_HEAD_OVERLAY_PNG_LEAGUE_LEADERBOARD.value \
             if self.is_league_leaderboard else TrackAssetsIconographicPngs.HEAD_TO_HEAD_OVERLAY_PNG.value
-        self.gradient_img = utils.get_image(gradient_img_path,
-                                            IconographicImageSize.FULL_IMAGE_SIZE.value)
-        self.gradient_alpha = self.gradient_img[:, :, 3] / 255.0
-
-        self.mp4_video_metrics_srv_list = list()
-        for racecar_info in self.racecars_info:
-            agent_name = 'agent' if len(racecars_info) == 1 else "agent_{}".format(racecar_info['name'].split("_")[1])
-            rospy.wait_for_service("/{}/{}".format(agent_name, "mp4_video_metrics"))
-            self.mp4_video_metrics_srv_list.append(ServiceProxyWrapper(
-                "/{}/{}".format(agent_name, "mp4_video_metrics"), VideoMetricsSrv))
+        self.gradient_img = self._plot_track_on_gradient(gradient_img_path)
+        self.gradient_alpha_rgb_mul, self.one_minus_gradient_alpha = utils.get_gradient_values(self.gradient_img)
 
         # Top camera information
         top_camera_info = utils.get_top_camera_info()
-        self.edited_topview_pub = rospy.Publisher('/deepracer/topview_stream', ROSImg, queue_size=1)
         self.top_view_graphics = TopViewGraphics(top_camera_info.horizontal_fov, top_camera_info.padding_pct,
                                                  top_camera_info.image_width, top_camera_info.image_height,
                                                  racecars_info)
 
-    def _edit_major_cv_image(self, major_cv_image):
+    def _edit_major_cv_image(self, major_cv_image, mp4_video_metrics_info):
         """ Apply all the editing for the Major 45degree camera image
         Args:
             major_cv_image (Image): Image straight from the camera
@@ -79,13 +73,8 @@ class MultiAgentImageEditing(ImageEditingInterface):
             Image: Edited main camera image
         """
         # Applying gradient to whole major image and then writing text
-        major_cv_image = utils.apply_gradient(major_cv_image, self.gradient_img, self.gradient_alpha)
-
-        # Subscribing to the agent metrics
-        mp4_video_metrics_info = list()
-        for racecar_info, mp4_video_metrics_srv in zip(self.racecars_info, self.mp4_video_metrics_srv_list):
-            mp4_video_metrics = mp4_video_metrics_srv(VideoMetricsSrvRequest())
-            mp4_video_metrics_info.append(mp4_video_metrics)
+        major_cv_image = utils.apply_gradient(major_cv_image, self.gradient_alpha_rgb_mul,
+                                              self.one_minus_gradient_alpha)
 
         # Adding display name to the image
         agents_speed = 0
@@ -177,20 +166,60 @@ class MultiAgentImageEditing(ImageEditingInterface):
             y_offset = major_cv_image.shape[0] - RACE_COMPLETE_Y_OFFSET - rel_y_offset - racecomplete_image.shape[0]//2
             major_cv_image = utils.plot_rectangular_image_on_main_image(
                 major_cv_image, racecomplete_image, (x_offset, y_offset))
-
+        major_cv_image = cv2.cvtColor(major_cv_image, cv2.COLOR_RGB2BGRA)
         return major_cv_image
 
-    def _edit_minor_cv_image(self):
-        """ Apply all the editing for the iconographic of the top view camera image
-        Returns:
-            Image: Edited image of the iconographic view of the top camera
-        """
-        # Edit the top camera image to have circles instead of dots
-        return self.top_view_graphics.plot_agents_as_circles(self.track_icongraphy_img.copy())
+    def _plot_track_on_gradient(self, gradient_img_path):
+        """ For the given gradient apply the track iconographic image and use this to apply gradient
+        on each camera frame. Previously this was done on the top camera which changed every frame. But
+        with the track iconographic image set static, adding the track on gradient is more optimized.
 
-    def edit_image(self, major_cv_image):
-        major_cv_image = self._edit_major_cv_image(major_cv_image)
-        minor_cv_image = self._edit_minor_cv_image()
-        offset_loc = XYPixelLoc.TRACK_IMG_WITH_OFFSET_LOC.value if self.is_league_leaderboard \
-            else XYPixelLoc.TRACK_IMG_WITHOUT_OFFSET_LOC.value
-        return utils.overlay_track_images(major_cv_image, minor_cv_image, offset_loc)
+        Arguments:
+            gradient_img_path (str): Gradient image path
+
+        Returns:
+            (Image): Edited gradient image with track image
+        """
+        gradient_img = utils.get_image(gradient_img_path, IconographicImageSize.FULL_IMAGE_SIZE.value)
+        gradient_img = cv2.cvtColor(gradient_img, cv2.COLOR_RGBA2BGRA)
+
+        track_icongraphy_scaled = utils.resize_image(self.track_icongraphy_img, SCALE_RATIO)
+        track_icongraphy_alpha = track_icongraphy_scaled[:, :, 3]/255.0
+
+        # Track image is placed at the bottom right with some offset (only in leaderboard tracks)
+        x_min = -(self.track_loc_offset[1] + track_icongraphy_scaled.shape[0])
+        x_max = gradient_img.shape[0] - self.track_loc_offset[1]
+        y_min = -(self.track_loc_offset[0] + track_icongraphy_scaled.shape[1])
+        y_max = gradient_img.shape[1] - self.track_loc_offset[0]
+
+        # This is used as the offset for plotting the agent dots
+        self.track_start_loc = (gradient_img.shape[1] + y_min, gradient_img.shape[0] + x_min)
+
+        for channel in range(0, 4):
+            gradient_img[x_min:x_max, y_min:y_max, channel] =\
+                (track_icongraphy_alpha * track_icongraphy_scaled[:, :, channel]) + \
+                (1 - track_icongraphy_alpha) * (gradient_img[x_min:x_max, y_min:y_max, channel])
+        return gradient_img
+
+    def _plot_agents_on_major_cv_image(self, major_cv_image, mp4_video_metrics_info):
+        """ Add the agents, obstacles on the track.
+
+        Arguments:
+            major_cv_image (Image): Edited image having gradient, text, track
+            mp4_video_metrics_info (List): List of ROS metric values of each agent
+
+        Returns:
+            Image: Edited image with gradient, text, track and agents with dots
+        """
+        agents_loc = [(metric.x, metric.y) for metric in mp4_video_metrics_info]
+        objects_loc = []
+        if mp4_video_metrics_info[0].object_locations:
+            objects_loc = [(object_loc.x, object_loc.y) for object_loc in mp4_video_metrics_info[0].object_locations]
+        return self.top_view_graphics.plot_agents_as_circles(
+            major_cv_image, agents_loc, objects_loc, self.track_start_loc)
+
+    def edit_image(self, major_cv_image, metric_info):
+        mp4_video_metrics_info = metric_info[FrameQueueData.AGENT_METRIC_INFO.value]
+        major_cv_image = self._edit_major_cv_image(major_cv_image, mp4_video_metrics_info)
+        major_cv_image = self._plot_agents_on_major_cv_image(major_cv_image, mp4_video_metrics_info)
+        return cv2.cvtColor(major_cv_image, cv2.COLOR_BGRA2RGB)
--- bundle/deepracer_simulation_environment/lib/python2.7/dist-packages/mp4_saving/single_agent_image_editing.py
+++ bundle/deepracer_simulation_environment/lib/python2.7/dist-packages/mp4_saving/single_agent_image_editing.py
@@ -5,15 +5,15 @@ import datetime
 import logging
 import rospy
 import cv2
-from sensor_msgs.msg import Image as ROSImg
+
 from markov.log_handler.logger import Logger
-from markov.rospy_wrappers import ServiceProxyWrapper
-from deepracer_simulation_environment.srv import VideoMetricsSrvRequest, VideoMetricsSrv
+from markov.utils import get_racecar_idx
 from mp4_saving import utils
 from mp4_saving.constants import (RaceCarColorToRGB,
                                   IconographicImageSize,
                                   TrackAssetsIconographicPngs, RACE_COMPLETE_Y_OFFSET,
-                                  RACE_TYPE_TO_VIDEO_TEXT_MAPPING, XYPixelLoc, AWS_DEEPRACER_WATER_MARK)
+                                  RACE_TYPE_TO_VIDEO_TEXT_MAPPING, XYPixelLoc, AWS_DEEPRACER_WATER_MARK,
+                                  SCALE_RATIO, FrameQueueData)
 from mp4_saving.image_editing_interface import ImageEditingInterface
 from mp4_saving.top_view_graphics import TopViewGraphics
 
@@ -23,7 +23,7 @@ class SingleAgentImageEditing(ImageEditingInterface):
     """ Image editing class for head to bot, time-trail, obstacle where
     there is only single agent
     """
-    def __init__(self, racecar_info, race_type):
+    def __init__(self, racecar_name, racecar_info, race_type):
         """ Initializing the required data for the head to bot, time-trail. This is used for single agent
         Arguments:
             racecars_info (list): list of dict having information of the agent
@@ -31,6 +31,8 @@ class SingleAgentImageEditing(ImageEditingInterface):
         """
         self.racecar_info = racecar_info
         self.race_type = race_type
+        racecar_index = get_racecar_idx(racecar_name)
+        self.racecar_index = racecar_index if racecar_index else 0
         # Store the font which we will use to write the phase with
         self.amazon_ember_regular_20px = utils.get_font('AmazonEmber-Regular', 20)
         self.amazon_ember_regular_16px = utils.get_font('AmazonEmber-Regular', 16)
@@ -45,24 +47,24 @@ class SingleAgentImageEditing(ImageEditingInterface):
 
         # The track image as iconography
         self.track_icongraphy_img = utils.get_track_iconography_image()
+
+        # Track image offset
+        self.track_loc_offset = XYPixelLoc.TRACK_IMG_WITH_OFFSET_LOC.value if self.is_league_leaderboard \
+            else XYPixelLoc.TRACK_IMG_WITHOUT_OFFSET_LOC.value
+
+        # Gradient overlay image
         gradient_img_path = TrackAssetsIconographicPngs.OBSTACLE_OVERLAY_PNG_LEAGUE_LEADERBOARD.value \
             if self.is_league_leaderboard else TrackAssetsIconographicPngs.OBSTACLE_OVERLAY_PNG.value
-        self.gradient_img = utils.get_image(gradient_img_path,
-                                            IconographicImageSize.FULL_IMAGE_SIZE.value)
-        self.gradient_alpha = self.gradient_img[:, :, 3] / 255.0
-
-        # Subscribing to the agent metrics
-        rospy.wait_for_service("/agent/mp4_video_metrics")
-        self.mp4_video_metrics_srv = ServiceProxyWrapper("/agent/mp4_video_metrics", VideoMetricsSrv)
+        self.gradient_img = self._plot_track_on_gradient(gradient_img_path)
+        self.gradient_alpha_rgb_mul, self.one_minus_gradient_alpha = utils.get_gradient_values(self.gradient_img)
 
         # Top camera information
         top_camera_info = utils.get_top_camera_info()
-        self.edited_topview_pub = rospy.Publisher('/deepracer/topview_stream', ROSImg, queue_size=1)
         self.top_view_graphics = TopViewGraphics(top_camera_info.horizontal_fov, top_camera_info.padding_pct,
                                                  top_camera_info.image_width, top_camera_info.image_height,
                                                  racecar_info)
 
-    def _edit_major_cv_image(self, major_cv_image):
+    def _edit_major_cv_image(self, major_cv_image, mp4_video_metrics_info):
         """ Apply all the editing for the Major 45degree camera image
         Args:
             major_cv_image (Image): Image straight from the camera
@@ -70,15 +72,14 @@ class SingleAgentImageEditing(ImageEditingInterface):
             Image: Edited main camera image
         """
         # Applying gradient to whole major image and then writing text
-        major_cv_image = utils.apply_gradient(major_cv_image, self.gradient_img, self.gradient_alpha)
-
-        mp4_video_metrics_info = self.mp4_video_metrics_srv(VideoMetricsSrvRequest())
+        major_cv_image = utils.apply_gradient(major_cv_image, self.gradient_alpha_rgb_mul,
+                                              self.one_minus_gradient_alpha)
 
         # Top left location of the picture
         loc_x, loc_y = XYPixelLoc.SINGLE_AGENT_DISPLAY_NAME_LOC.value
 
         # Display name (Racer name/Model name)
-        display_name = self.racecar_info['display_name']
+        display_name = self.racecar_info[self.racecar_index]['display_name']
         display_name_txt = display_name if len(display_name) < 15 else "{}...".format(display_name[:15])
         major_cv_image = utils.write_text_on_image(image=major_cv_image, text=display_name_txt,
                                                    loc=(loc_x, loc_y), font=self.amazon_ember_regular_20px,
@@ -87,7 +88,7 @@ class SingleAgentImageEditing(ImageEditingInterface):
         # Lap Counter
         loc_y += 30
         total_laps = rospy.get_param("NUMBER_OF_TRIALS", 0)
-        current_lap = int(mp4_video_metrics_info.lap_counter) + 1
+        current_lap = min(int(mp4_video_metrics_info[self.racecar_index].lap_counter) + 1, total_laps)
         lap_counter_text = "{}/{}".format(current_lap, total_laps)
         major_cv_image = utils.write_text_on_image(image=major_cv_image, text=lap_counter_text,
                                                    loc=(loc_x, loc_y), font=self.amazon_ember_heavy_30px,
@@ -95,7 +96,7 @@ class SingleAgentImageEditing(ImageEditingInterface):
                                                    font_shadow_color=RaceCarColorToRGB.Black.value)
         # total_evaluation_time (Race time)
         loc_y += 45
-        total_eval_milli_seconds = mp4_video_metrics_info.total_evaluation_time
+        total_eval_milli_seconds = mp4_video_metrics_info[self.racecar_index].total_evaluation_time
         time_delta = datetime.timedelta(milliseconds=total_eval_milli_seconds)
         total_eval_time_text = "Race | {}".format(utils.milliseconds_to_timeformat(time_delta))
         major_cv_image = utils.write_text_on_image(image=major_cv_image, text=total_eval_time_text,
@@ -104,7 +105,7 @@ class SingleAgentImageEditing(ImageEditingInterface):
                                                    font_shadow_color=RaceCarColorToRGB.Black.value)
         # Reset counter
         loc_y += 25
-        reset_counter_text = "Reset | {}".format(mp4_video_metrics_info.reset_counter)
+        reset_counter_text = "Reset | {}".format(mp4_video_metrics_info[self.racecar_index].reset_counter)
         major_cv_image = utils.write_text_on_image(image=major_cv_image, text=reset_counter_text,
                                                    loc=(loc_x, loc_y), font=self.amazon_ember_light_18px,
                                                    font_color=RaceCarColorToRGB.White.value,
@@ -113,7 +114,7 @@ class SingleAgentImageEditing(ImageEditingInterface):
         loc_x, loc_y = XYPixelLoc.SPEED_EVAL_LOC.value
         if self.is_league_leaderboard:
             loc_x, loc_y = XYPixelLoc.SPEED_LEADERBOARD_LOC.value
-        speed_text = "{} m/s".format(utils.get_speed_formatted_str(mp4_video_metrics_info.throttle))
+        speed_text = "{} m/s".format(utils.get_speed_formatted_str(mp4_video_metrics_info[self.racecar_index].throttle))
         major_cv_image = utils.write_text_on_image(image=major_cv_image, text=speed_text,
                                                    loc=(loc_x, loc_y), font=self.amazon_ember_light_20px,
                                                    font_color=RaceCarColorToRGB.White.value,
@@ -145,28 +146,69 @@ class SingleAgentImageEditing(ImageEditingInterface):
                                                        font_shadow_color=RaceCarColorToRGB.Black.value)
 
         # Check if the done flag is set and set the banner appropriately
-        if mp4_video_metrics_info.done and (int(total_laps) == current_lap):
+        if mp4_video_metrics_info[self.racecar_index].done and (int(total_laps) >= current_lap):
             # When the cv2 text is written, it automatically drops the alpha value of the image
             rel_y_offset = XYPixelLoc.TRACK_IMG_WITH_OFFSET_LOC.value[1] if self.is_league_leaderboard else 0
-            major_cv_image = cv2.cvtColor(major_cv_image, cv2.COLOR_RGB2RGBA)
             racecomplete_image = utils.get_image(TrackAssetsIconographicPngs.RACE_COMPLETE_OVERLAY_PNG.value,
                                                  IconographicImageSize.RACE_COMPLETE_IMAGE_SIZE.value)
             x_offset = major_cv_image.shape[1] - racecomplete_image.shape[1]//2
             y_offset = major_cv_image.shape[0] - RACE_COMPLETE_Y_OFFSET - rel_y_offset - racecomplete_image.shape[0]//2
             major_cv_image = utils.plot_rectangular_image_on_main_image(
                 major_cv_image, racecomplete_image, (x_offset, y_offset))
+        major_cv_image = cv2.cvtColor(major_cv_image, cv2.COLOR_RGB2BGRA)
         return major_cv_image
 
-    def _edit_minor_cv_image(self):
-        """ Apply all the editing for the iconographic of the top view camera image
-        Returns:
-            Image: Edited image of the iconographic view of the top camera
-        """
-        return self.top_view_graphics.plot_agents_as_circles(self.track_icongraphy_img.copy())
+    def _plot_track_on_gradient(self, gradient_img_path):
+        """ For the given gradient apply the track iconographic image and use this to apply gradient
+        on each camera frame. Previously this was done on the top camera which changed every frame. But
+        with the track iconographic image set static, adding the track on gradient is more optimized.
 
-    def edit_image(self, major_cv_image):
-        major_cv_image = self._edit_major_cv_image(major_cv_image)
-        minor_cv_image = self._edit_minor_cv_image()
-        offset_loc = XYPixelLoc.TRACK_IMG_WITH_OFFSET_LOC.value if self.is_league_leaderboard \
-            else XYPixelLoc.TRACK_IMG_WITHOUT_OFFSET_LOC.value
-        return utils.overlay_track_images(major_cv_image, minor_cv_image, offset_loc)
+        Arguments:
+            gradient_img_path (str): Gradient image path
+
+        Returns:
+            (Image): Edited gradient image with track image
+        """
+        gradient_img = utils.get_image(gradient_img_path, IconographicImageSize.FULL_IMAGE_SIZE.value)
+        gradient_img = cv2.cvtColor(gradient_img, cv2.COLOR_RGBA2BGRA)
+
+        track_icongraphy_scaled = utils.resize_image(self.track_icongraphy_img, SCALE_RATIO)
+        track_icongraphy_alpha = track_icongraphy_scaled[:, :, 3]/255.0
+
+        # Track image is placed at the bottom right with some offset (only in leaderboard tracks)
+        x_min = -(self.track_loc_offset[1] + track_icongraphy_scaled.shape[0])
+        x_max = gradient_img.shape[0] - self.track_loc_offset[1]
+        y_min = -(self.track_loc_offset[0] + track_icongraphy_scaled.shape[1])
+        y_max = gradient_img.shape[1] - self.track_loc_offset[0]
+
+        # This is used as the offset for plotting the agent dots
+        self.track_start_loc = (gradient_img.shape[1] + y_min, gradient_img.shape[0] + x_min)
+
+        for channel in range(0, 4):
+            gradient_img[x_min:x_max, y_min:y_max, channel] =\
+                (track_icongraphy_alpha * track_icongraphy_scaled[:, :, channel]) + \
+                (1 - track_icongraphy_alpha) * (gradient_img[x_min:x_max, y_min:y_max, channel])
+        return gradient_img
+
+    def _plot_agents_on_major_cv_image(self, major_cv_image, mp4_video_metrics_info):
+        """ Add the agents, obstacles on the track.
+
+        Arguments:
+            major_cv_image (Image): Edited image having gradient, text, track
+            mp4_video_metrics_info (List): List of ROS metric values of each agent
+
+        Returns:
+            Image: Edited image with gradient, text, track and agents with dots
+        """
+        agents_loc = [(metric.x, metric.y) for metric in mp4_video_metrics_info]
+        objects_loc = []
+        if mp4_video_metrics_info[0].object_locations:
+            objects_loc = [(object_loc.x, object_loc.y) for object_loc in mp4_video_metrics_info[0].object_locations]
+        return self.top_view_graphics.plot_agents_as_circles(
+            major_cv_image, agents_loc, objects_loc, self.track_start_loc)
+
+    def edit_image(self, major_cv_image, metric_info):
+        mp4_video_metrics_info = metric_info[FrameQueueData.AGENT_METRIC_INFO.value]
+        major_cv_image = self._edit_major_cv_image(major_cv_image, mp4_video_metrics_info)
+        major_cv_image = self._plot_agents_on_major_cv_image(major_cv_image, mp4_video_metrics_info)
+        return cv2.cvtColor(major_cv_image, cv2.COLOR_BGRA2RGB)
