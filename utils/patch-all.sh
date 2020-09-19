f191d14929c8aac00aacf01440ceeecd0095b5bb
Lars Lorentz Ludvigsen <larsll@outlook.com>
2020-09-19 08:54:43 +0000

    Adding patching utility

---
+++ utils/patch-all.sh
@@ -0,0 +1,6 @@
+VERS="cpu-avx cpu-avx2 cpu-gl-avx cpu-gl-avx2 gpu gpu-gl"
+BASE="3.0.1"
+TOBE=`cat VERSION`
+for a in $VERS; do
+	docker build . -t awsdeepracercommunity/deepracer-robomaker:$TOBE-$a -f docker/Dockerfile.localext --build-arg FROM_TAG=$BASE-$a
+done
