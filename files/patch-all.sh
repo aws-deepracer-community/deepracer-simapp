5ba79da1d6110c6f5a1fdc64cd95540b423965cd
Lars Lorentz Ludvigsen <larsll@outlook.com>
2020-11-10 08:17:40 +0000

    Fixing build files

--- files/patch-all.sh
+++ files/patch-all.sh
@@ -1,5 +1,5 @@
 VERS="cpu-avx cpu-avx2 cpu-gl-avx cpu-gl-avx2 gpu gpu-gl"
-BASE="2.0.11"
+BASE="2.0.13"
 TOBE=`cat VERSION`
 for a in $VERS; do
 	docker build . -t awsdeepracercommunity/deepracer-robomaker:$TOBE-$a -f docker/Dockerfile.localext --build-arg FROM_TAG=$BASE-$a
