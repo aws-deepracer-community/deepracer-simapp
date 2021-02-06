cbd0760406de9590754b9dbaf0c4d56021fb462a
Lars Lorentz Ludvigsen <larsll@outlook.com>
2021-02-06 10:43:28 +0000

    Update dates

--- utils/extract-and-diff.sh
+++ utils/extract-and-diff.sh
@@ -2,8 +2,8 @@
 cd bundle-src
 aws s3 cp s3://deepracer-managed-resources-us-east-1/deepracer-simapp.tar.gz .
 tar xvf deepracer-simapp.tar.gz bundle.tar
-CUR_DATE="nov-15"
-PREV_DATE="nov-10"
+CUR_DATE="jan-29"
+PREV_DATE="dec-16"
 mkdir "bundle-$CUR_DATE" && cd "bundle-$CUR_DATE"
 tar xvf ../bundle.tar opt/install
 find . -name __pycache__ | xargs rm -rf
