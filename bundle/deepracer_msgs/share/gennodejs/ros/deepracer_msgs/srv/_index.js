c85a7df26e83fa074353fea57b3028c8ddb55ce5
Lars Lorentz Ludvigsen <larsll@outlook.com>
2021-08-29 15:02:24 +0000

    Minor bundle updates

--- bundle/deepracer_msgs/share/gennodejs/ros/deepracer_msgs/srv/_index.js
+++ bundle/deepracer_msgs/share/gennodejs/ros/deepracer_msgs/srv/_index.js
@@ -1,42 +1,42 @@
 
 "use strict";
 
-let SetVisualPoses = require('./SetVisualPoses.js')
-let SetVisualColors = require('./SetVisualColors.js')
-let SetVisualColor = require('./SetVisualColor.js')
-let SetVisualVisible = require('./SetVisualVisible.js')
-let SetLinkStates = require('./SetLinkStates.js')
-let SetVisualPose = require('./SetVisualPose.js')
-let SetVisualMeshes = require('./SetVisualMeshes.js')
-let SetModelStates = require('./SetModelStates.js')
-let SetVisualTransparencies = require('./SetVisualTransparencies.js')
-let SetVisualVisibles = require('./SetVisualVisibles.js')
-let SetVisualTransparency = require('./SetVisualTransparency.js')
-let SetVisualMesh = require('./SetVisualMesh.js')
-let GetVisuals = require('./GetVisuals.js')
-let GetLinkStates = require('./GetLinkStates.js')
 let GetVisualNames = require('./GetVisualNames.js')
+let GetVisuals = require('./GetVisuals.js')
+let SetVisualPoses = require('./SetVisualPoses.js')
 let GetModelStates = require('./GetModelStates.js')
+let SetModelStates = require('./SetModelStates.js')
+let SetVisualTransparency = require('./SetVisualTransparency.js')
+let SetVisualMeshes = require('./SetVisualMeshes.js')
+let SetVisualVisibles = require('./SetVisualVisibles.js')
+let SetVisualPose = require('./SetVisualPose.js')
 let GetLightNames = require('./GetLightNames.js')
+let SetVisualVisible = require('./SetVisualVisible.js')
+let SetVisualTransparencies = require('./SetVisualTransparencies.js')
+let GetLinkStates = require('./GetLinkStates.js')
+let SetLinkStates = require('./SetLinkStates.js')
+let SetVisualMesh = require('./SetVisualMesh.js')
 let GetVisual = require('./GetVisual.js')
+let SetVisualColor = require('./SetVisualColor.js')
+let SetVisualColors = require('./SetVisualColors.js')
 
 module.exports = {
-  SetVisualPoses: SetVisualPoses,
-  SetVisualColors: SetVisualColors,
-  SetVisualColor: SetVisualColor,
-  SetVisualVisible: SetVisualVisible,
-  SetLinkStates: SetLinkStates,
-  SetVisualPose: SetVisualPose,
-  SetVisualMeshes: SetVisualMeshes,
-  SetModelStates: SetModelStates,
-  SetVisualTransparencies: SetVisualTransparencies,
-  SetVisualVisibles: SetVisualVisibles,
-  SetVisualTransparency: SetVisualTransparency,
-  SetVisualMesh: SetVisualMesh,
-  GetVisuals: GetVisuals,
-  GetLinkStates: GetLinkStates,
   GetVisualNames: GetVisualNames,
+  GetVisuals: GetVisuals,
+  SetVisualPoses: SetVisualPoses,
   GetModelStates: GetModelStates,
+  SetModelStates: SetModelStates,
+  SetVisualTransparency: SetVisualTransparency,
+  SetVisualMeshes: SetVisualMeshes,
+  SetVisualVisibles: SetVisualVisibles,
+  SetVisualPose: SetVisualPose,
   GetLightNames: GetLightNames,
+  SetVisualVisible: SetVisualVisible,
+  SetVisualTransparencies: SetVisualTransparencies,
+  GetLinkStates: GetLinkStates,
+  SetLinkStates: SetLinkStates,
+  SetVisualMesh: SetVisualMesh,
   GetVisual: GetVisual,
+  SetVisualColor: SetVisualColor,
+  SetVisualColors: SetVisualColors,
 };
--- bundle/deepracer_simulation_environment/share/gennodejs/ros/deepracer_simulation_environment/srv/_index.js
+++ bundle/deepracer_simulation_environment/share/gennodejs/ros/deepracer_simulation_environment/srv/_index.js
@@ -1,12 +1,12 @@
 
 "use strict";
 
-let TopCamDataSrv = require('./TopCamDataSrv.js')
-let VideoMetricsSrv = require('./VideoMetricsSrv.js')
 let VirtualEventVideoEditSrv = require('./VirtualEventVideoEditSrv.js')
+let VideoMetricsSrv = require('./VideoMetricsSrv.js')
+let TopCamDataSrv = require('./TopCamDataSrv.js')
 
 module.exports = {
-  TopCamDataSrv: TopCamDataSrv,
-  VideoMetricsSrv: VideoMetricsSrv,
   VirtualEventVideoEditSrv: VirtualEventVideoEditSrv,
+  VideoMetricsSrv: VideoMetricsSrv,
+  TopCamDataSrv: TopCamDataSrv,
 };
