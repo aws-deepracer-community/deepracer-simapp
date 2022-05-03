20cd2a43c88b3649211efcc7cb18514b78862de8
Lars Lorentz Ludvigsen <larsll@outlook.com>
2022-05-03 17:42:11 +0000

    Minor edits

--- bundle/deepracer_msgs/share/gennodejs/ros/deepracer_msgs/srv/_index.js
+++ bundle/deepracer_msgs/share/gennodejs/ros/deepracer_msgs/srv/_index.js
@@ -1,42 +1,42 @@
 
 "use strict";
 
-let GetVisualNames = require('./GetVisualNames.js')
-let GetLinkStates = require('./GetLinkStates.js')
-let GetVisual = require('./GetVisual.js')
-let GetVisuals = require('./GetVisuals.js')
-let SetVisualVisibles = require('./SetVisualVisibles.js')
-let SetVisualColors = require('./SetVisualColors.js')
+let GetLightNames = require('./GetLightNames.js')
 let SetLinkStates = require('./SetLinkStates.js')
-let SetVisualMeshes = require('./SetVisualMeshes.js')
-let SetVisualColor = require('./SetVisualColor.js')
-let SetVisualPose = require('./SetVisualPose.js')
-let SetVisualTransparency = require('./SetVisualTransparency.js')
-let SetVisualPoses = require('./SetVisualPoses.js')
-let SetVisualVisible = require('./SetVisualVisible.js')
 let GetModelStates = require('./GetModelStates.js')
+let SetVisualColors = require('./SetVisualColors.js')
+let GetVisual = require('./GetVisual.js')
+let SetVisualPoses = require('./SetVisualPoses.js')
+let SetVisualColor = require('./SetVisualColor.js')
+let SetVisualMesh = require('./SetVisualMesh.js')
+let GetVisualNames = require('./GetVisualNames.js')
+let SetVisualVisibles = require('./SetVisualVisibles.js')
+let GetVisuals = require('./GetVisuals.js')
+let SetVisualTransparency = require('./SetVisualTransparency.js')
 let SetModelStates = require('./SetModelStates.js')
 let SetVisualTransparencies = require('./SetVisualTransparencies.js')
-let GetLightNames = require('./GetLightNames.js')
-let SetVisualMesh = require('./SetVisualMesh.js')
+let SetVisualMeshes = require('./SetVisualMeshes.js')
+let SetVisualPose = require('./SetVisualPose.js')
+let GetLinkStates = require('./GetLinkStates.js')
+let SetVisualVisible = require('./SetVisualVisible.js')
 
 module.exports = {
-  GetVisualNames: GetVisualNames,
-  GetLinkStates: GetLinkStates,
-  GetVisual: GetVisual,
-  GetVisuals: GetVisuals,
-  SetVisualVisibles: SetVisualVisibles,
-  SetVisualColors: SetVisualColors,
+  GetLightNames: GetLightNames,
   SetLinkStates: SetLinkStates,
-  SetVisualMeshes: SetVisualMeshes,
-  SetVisualColor: SetVisualColor,
-  SetVisualPose: SetVisualPose,
-  SetVisualTransparency: SetVisualTransparency,
-  SetVisualPoses: SetVisualPoses,
-  SetVisualVisible: SetVisualVisible,
   GetModelStates: GetModelStates,
+  SetVisualColors: SetVisualColors,
+  GetVisual: GetVisual,
+  SetVisualPoses: SetVisualPoses,
+  SetVisualColor: SetVisualColor,
+  SetVisualMesh: SetVisualMesh,
+  GetVisualNames: GetVisualNames,
+  SetVisualVisibles: SetVisualVisibles,
+  GetVisuals: GetVisuals,
+  SetVisualTransparency: SetVisualTransparency,
   SetModelStates: SetModelStates,
   SetVisualTransparencies: SetVisualTransparencies,
-  GetLightNames: GetLightNames,
-  SetVisualMesh: SetVisualMesh,
+  SetVisualMeshes: SetVisualMeshes,
+  SetVisualPose: SetVisualPose,
+  GetLinkStates: GetLinkStates,
+  SetVisualVisible: SetVisualVisible,
 };
