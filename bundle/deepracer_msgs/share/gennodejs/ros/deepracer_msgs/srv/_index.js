942578683c7cbe11b88ec43a6d5965f05e121da1
Lars Lorentz Ludvigsen <larsll@outlook.com>
2022-09-02 06:27:08 +0000

    Minor changes from AWS

--- bundle/deepracer_msgs/share/gennodejs/ros/deepracer_msgs/srv/_index.js
+++ bundle/deepracer_msgs/share/gennodejs/ros/deepracer_msgs/srv/_index.js
@@ -1,42 +1,42 @@
 
 "use strict";
 
-let GetLightNames = require('./GetLightNames.js')
-let SetLinkStates = require('./SetLinkStates.js')
-let GetModelStates = require('./GetModelStates.js')
 let SetVisualColors = require('./SetVisualColors.js')
-let GetVisual = require('./GetVisual.js')
 let SetVisualPoses = require('./SetVisualPoses.js')
-let SetVisualColor = require('./SetVisualColor.js')
+let GetModelStates = require('./GetModelStates.js')
 let SetVisualMesh = require('./SetVisualMesh.js')
-let GetVisualNames = require('./GetVisualNames.js')
 let SetVisualVisibles = require('./SetVisualVisibles.js')
-let GetVisuals = require('./GetVisuals.js')
-let SetVisualTransparency = require('./SetVisualTransparency.js')
-let SetModelStates = require('./SetModelStates.js')
-let SetVisualTransparencies = require('./SetVisualTransparencies.js')
-let SetVisualMeshes = require('./SetVisualMeshes.js')
-let SetVisualPose = require('./SetVisualPose.js')
 let GetLinkStates = require('./GetLinkStates.js')
+let GetVisual = require('./GetVisual.js')
+let SetLinkStates = require('./SetLinkStates.js')
+let GetVisuals = require('./GetVisuals.js')
+let SetVisualTransparencies = require('./SetVisualTransparencies.js')
+let GetVisualNames = require('./GetVisualNames.js')
+let SetVisualTransparency = require('./SetVisualTransparency.js')
+let SetVisualPose = require('./SetVisualPose.js')
+let SetModelStates = require('./SetModelStates.js')
 let SetVisualVisible = require('./SetVisualVisible.js')
+let SetVisualMeshes = require('./SetVisualMeshes.js')
+let GetLightNames = require('./GetLightNames.js')
+let SetVisualColor = require('./SetVisualColor.js')
 
 module.exports = {
-  GetLightNames: GetLightNames,
-  SetLinkStates: SetLinkStates,
-  GetModelStates: GetModelStates,
   SetVisualColors: SetVisualColors,
-  GetVisual: GetVisual,
   SetVisualPoses: SetVisualPoses,
-  SetVisualColor: SetVisualColor,
+  GetModelStates: GetModelStates,
   SetVisualMesh: SetVisualMesh,
-  GetVisualNames: GetVisualNames,
   SetVisualVisibles: SetVisualVisibles,
-  GetVisuals: GetVisuals,
-  SetVisualTransparency: SetVisualTransparency,
-  SetModelStates: SetModelStates,
-  SetVisualTransparencies: SetVisualTransparencies,
-  SetVisualMeshes: SetVisualMeshes,
-  SetVisualPose: SetVisualPose,
   GetLinkStates: GetLinkStates,
+  GetVisual: GetVisual,
+  SetLinkStates: SetLinkStates,
+  GetVisuals: GetVisuals,
+  SetVisualTransparencies: SetVisualTransparencies,
+  GetVisualNames: GetVisualNames,
+  SetVisualTransparency: SetVisualTransparency,
+  SetVisualPose: SetVisualPose,
+  SetModelStates: SetModelStates,
   SetVisualVisible: SetVisualVisible,
+  SetVisualMeshes: SetVisualMeshes,
+  GetLightNames: GetLightNames,
+  SetVisualColor: SetVisualColor,
 };
--- bundle/deepracer_simulation_environment/lib/deepracer_simulation_environment/run_virtual_event_rl_agent.sh
+++ bundle/deepracer_simulation_environment/lib/deepracer_simulation_environment/run_virtual_event_rl_agent.sh
@@ -5,4 +5,9 @@ set -ex
 #export PYTHONPATH=${COLCON_BUNDLE_PREFIX}/usr/local/lib/python3.6/dist-packages/:$PYTHONPATH
 export PYTHONUNBUFFERED=1
 
+if [[ -n "${HEARTBEAT_S3_LOCATION}" || "${HEARTBEAT_CLOUDWATCH_METRICS}" == "1" ]]; then
+    python3 /opt/amazon/script/start_deepracer_node_monitor.py \
+        --node_monitor_file_path /opt/amazon/src/deepracer_node_monitor/config/deepracer_virtual_event_node_monitor_list.txt 2>&1 &
+fi
+
 python3 -m markov.virtual_event_worker
