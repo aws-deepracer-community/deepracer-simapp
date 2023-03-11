aa8e015bab4d4e054f49885e17e67bce69868632
Lars Lorentz Ludvigsen <larsll@outlook.com>
2023-03-11 17:10:19 +0000

    Delete generated files

--- bundle/deepracer_simulation_environment/share/common-lisp/ros/deepracer_simulation_environment/srv/_package_VirtualEventVideoEditSrv.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package deepracer_simulation_environment-srv)
-(cl:export '(DISPLAY_NAME-VAL
-          DISPLAY_NAME
-          RACECAR_COLOR-VAL
-          RACECAR_COLOR
-          SUCCESS-VAL
-          SUCCESS
-))
