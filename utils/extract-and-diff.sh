dbcd2100acbb1220f655a8941e8422fb0fa1df7a
Lars Lorentz Ludvigsen <larsll@outlook.com>
2023-07-04 18:42:41 +0000

    Updated extract and diff

--- utils/extract-and-diff.sh
+++ utils/extract-and-diff.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-cd bundle-src
-aws s3 cp s3://deepracer-melodic-managed-resources-us-east-1/deepracer-simapp.tar.gz .
-tar xvf deepracer-simapp.tar.gz bundle.tar
-CUR_DATE="2022-nov-19"
-PREV_DATE="2022-okt-01"
-mkdir "bundle-$CUR_DATE" && cd "bundle-$CUR_DATE"
-tar xvf ../bundle.tar opt/install
-find . -name __pycache__ | xargs rm -rf
-find . -name *.pyc | xargs rm -rf
-cd ..
-rm  deepracer-simapp.tar.gz bundle.tar
-diff -U 10 -Nr bundle-${PREV_DATE} bundle-${CUR_DATE}/ > compare-$CUR_DATE.diff
+mkdir -p /tmp/bundle-src
 
-cd ..
-cd bundle
-patch -p 3 < ../bundle-src/compare-$CUR_DATE.diff
-cd ..
-# Track binaries
-# cp -r bundle-src/bundle-$CUR_DATE/opt/install/deepracer_simulation_environment/share/deepracer_simulation_environment/track_iconography/2022_sep* bundle/deepracer_simulation_environment/share/deepracer_simulation_environment/track_iconography/
-# cp -r bundle-src/bundle-$CUR_DATE/opt/install/deepracer_simulation_environment/share/deepracer_simulation_environment/meshes/2022_sep* bundle/deepracer_simulation_environment/share/deepracer_simulation_environment/meshes
-# cp -r bundle-src/bundle-$CUR_DATE/opt/install/deepracer_simulation_environment/share/deepracer_simulation_environment/routes/2022_sep* bundle/deepracer_simulation_environment/share/deepracer_simulation_environment/routes
+git checkout upstream
+
+docker pull public.ecr.aws/k1d3r4z1/deepracer-sim-public:latest
+docker run -d -i --rm --name deepracer-simapp public.ecr.aws/k1d3r4z1/deepracer-sim-public bash
+docker export -o /tmp/bundle-src/deepracer-simapp-`date +%Y-%m-%d`.tar deepracer-simapp 
+docker stop deepracer-simapp
+
+cd /tmp/bundle-src
+tar xf deepracer-simapp-`date +%Y-%m-%d`.tar opt/amazon
+cd opt/amazon
+rm -rf build/ install/ log/
+
+cp -r * ~/deepracer-simapp/bundle/
+
