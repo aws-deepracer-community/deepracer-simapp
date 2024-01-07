7a0561f2571cc92e176040c69078e9cbff1f8f90
Lars Ludvigsen <larsll@outlook.com>
2024-01-07 15:43:15 +0100

    Node Monitor Fixes

--- bundle/src/deepracer_simulation_environment/scripts/run_evaluation_rl_agent.sh
+++ bundle/src/deepracer_simulation_environment/scripts/run_evaluation_rl_agent.sh
@@ -5,7 +5,7 @@ set -ex
 #export PYTHONPATH=${COLCON_BUNDLE_PREFIX}/usr/local/lib/python3.6/dist-packages/:$PYTHONPATH
 export PYTHONUNBUFFERED=1
 
-python3 /opt/amazon/script/start_deepracer_node_monitor.py \
-    --node_monitor_file_path /opt/amazon/src/deepracer_node_monitor/config/deepracer_node_monitor_list.txt 2>&1 &
+python3 /opt/install/start_deepracer_node_monitor.py \
+    --node_monitor_file_path /opt/install/deepracer_node_monitor_list.txt 2>&1 &
 
 python3 -m markov.evaluation_worker
