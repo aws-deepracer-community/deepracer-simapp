59b00d771a9da1b0c1f0fc677731349064e59265
Lars Ludvigsen <larsll@outlook.com>
2024-01-28 11:16:19 +0100

    Tuning

--- build-dev-bundle.sh
+++ build-dev-bundle.sh
@@ -8,11 +8,13 @@ function ctrl_c() {
 
 PREFIX="awsdeepracercommunity"
 
-while getopts ":cfp:" opt; do
+while getopts ":cgfp:" opt; do
 case $opt in
 c) OPT_CORE="core"
 ;;
 p) PREFIX="$OPTARG"
+;;
+g) OPT_GAZEBO="gazebo"
 ;;
 f) OPT_NOCACHE="--no-cache"
 ;;
@@ -35,4 +37,22 @@ echo "Preparing devel image for user $(id -u)..."
 docker buildx build ${OPT_NOCACHE} -t ${PREFIX}/deepracer-robomaker-build-devel:latest -f docker/Dockerfile.localuser --build-arg FROM_IMG=${PREFIX}/deepracer-robomaker-build-core:latest --build-arg USERNAME=$USER --build-arg USER_UID=$(id -u) --build-arg USER_GID=$(id -g) . 
 
 echo "Building development build of bundle into $(pwd)/bundle..."
-docker run --rm -ti -v $(pwd)/bundle:/opt/bundle ${PREFIX}/deepracer-robomaker-devel:latest bash -c 'colcon build'
+docker run --rm -ti -v $(pwd)/bundle:/opt/bundle ${PREFIX}/deepracer-robomaker-build-devel:latest bash -c 'colcon build'
+
+if [ -n "${OPT_GAZEBO}" ];
+then
+        if [ -z "${DR_ROBOMAKER_IMAGE}" ];
+        then
+                echo "No base image specified, please provide via DR_ROBOMAKER_IMAGE".
+                exit 1
+        fi
+
+        if [ -z "${DR_WORLD_NAME}" ];
+        then
+                echo "No world name specified, please provide via DR_WORLD_NAME".
+                exit 1
+        fi
+
+        echo "Starting Gazebo using awsdeepracercommunity/deepracer-robomaker:${DR_ROBOMAKER_IMAGE} with world ${DR_WORLD_NAME}."
+        USER_UID=$(id -u) USER_GID=$(id -g) docker compose -f docker/docker-compose-development.yml up --build
+fi
