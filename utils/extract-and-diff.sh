9853d4cfbab05e601cd3589049b6827be01775c8
Lars Ludvigsen <larsll@outlook.com>
2026-02-05 08:51:19 +0100

    Extract and diff updates

--- utils/extract-and-diff.sh
+++ utils/extract-and-diff.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e 
-git checkout upstream
+git checkout upstream-ros2
 
 mkdir -p /tmp/bundle-src
 
-docker pull public.ecr.aws/aws-solutions/deepracer-on-aws-simapp:v1.0.0
-docker run -d -i --rm --name deepracer-simapp public.ecr.aws/aws-solutions/deepracer-on-aws-simapp:v1.0.0 bash
+docker pull public.ecr.aws/aws-solutions/deepracer-on-aws-simapp:v1.0.1
+docker run -d -i --rm --name deepracer-simapp public.ecr.aws/aws-solutions/deepracer-on-aws-simapp:v1.0.1 bash
 docker cp deepracer-simapp:/opt/amazon /tmp/bundle-src
 docker cp deepracer-simapp:/opt/ml /tmp/bundle-src
 docker stop deepracer-simapp
@@ -14,4 +14,8 @@ cd /tmp/bundle-src/amazon
 rm -rf build/ install/ log/
 
 cp -r * ~/deepracer-simapp/bundle/
+
+cd /tmp/bundle-src/ml/code
+rm -rf python3.10/
+cp -r * ~/deepracer-simapp/docker/files/ml-code/
 
