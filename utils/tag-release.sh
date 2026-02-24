de3e0eae528834b28e96fcc6f134bb3919dcfd52
Sebastian Eresheim <sebastian.eresheim@fhstp.ac.at>
2026-02-24 14:42:40 +0100

    refactor root folder

--- utils/tag-release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-VERSION="2.0.11"
-SOURCE_TAG=$(docker images | grep $VERSION | awk '/deepracer-robomaker/ {print $2}')
-PREFIX="awsdeepracercommunity/deepracer-robomaker:"
-for TAG in $SOURCE_TAG; do
-    DEST_TAG=$(echo $TAG | cut -f2- -d\- )
-    docker tag ${PREFIX}${TAG} ${PREFIX}${DEST_TAG} 
-done
-docker tag ${PREFIX}cpu-avx ${PREFIX}cpu
